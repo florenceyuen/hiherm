265b7b7131fdac473550ab047ac57a9af9f24244
Florence Yuen <f4yuen@uwaterloo.ca>
2023-09-23 22:51:20 -0400

    Add dialog boxes and script which displays each line in a given text file

---
+++ Assets/TextImporter.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextImporter4 : MonoBehaviour
+{
+    public GameObject textBox;
+
+    public Text theText;
+
+    public TextAsset textFile;
+    public string[] textLines;
+
+    public int currentLine;
+    public int endAtLine;
+
+    // public PlayerController player;
+
+    void Start()
+    {
+        // player = findObjectOfType<PlayerController>();
+
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n')); // grab text file and split into separate for each new line
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+    }
+
+    void Update()
+    {
+
+        theText.text = textLines[currentLine];
+
+        if (currentLine > endAtLine)
+        {
+            textBox.SetActive(false);
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                currentLine += 1;
+            }
+        }
+    }
+}
